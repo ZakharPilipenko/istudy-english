1659982e92fcda69f760066675727cd447e9c9ac
alandosman <aland.o@the-gw.com>
2023-08-28 17:55:30 +0300

    fix sound and images

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,31 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({item, isVisible, isFinished, onCardClick}) => {
-  const {id, url, audio, description} = item;
-  const className = `${
-    isVisible ? 'card-show' : ''
-  } ${
-    isFinished ? 'card-finished' : ''
-  }`;
+const Card = ({ item, isVisible, isFinished, onCardClick }) => {
+  const { id, url, audio, description } = item;
+  const className = `${isVisible ? "card-show" : ""} ${isFinished ? "card-finished" : ""}`;
 
-  const playAudio = ({audio}) => {
+  const playAudio = (audio) => {
     const sound = new Audio(audio);
     sound.play();
-  }
+  };
 
   const handleClick = () => {
     playAudio(audio);
     onCardClick(id);
   };
-    return (
-      <li onClick={handleClick} className={`card ${className}`}>
-      <img
-        width="204" height="144"
-        src={url}
-        alt={description}
-      />
+  return (
+    <li onClick={handleClick} className={`card ${className}`}>
+      <img width="204" height="144" src={url} alt={description} />
     </li>
-    );
-}
+  );
+};
 
 export default Card;

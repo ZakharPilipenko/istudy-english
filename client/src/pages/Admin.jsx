3002ea59427cb0e53f021d6c3a3f0978f8c3aa7e
ZakharPilipenko <zaharpilipenko@gmail.com>
2023-09-06 13:11:52 +0300

    added card in the admin page
    The Card is added on the admin page to provide a better visual representation of the most recently added card.

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import CreateCard from "../components/UI/modals/CreateCard";
 import CreateCardType from "../components/UI/modals/CreateCardType";
 import CreateTheme from "../components/UI/modals/CreateTheme";
 import CreateLevel from "../components/UI/modals/CreateLevel";
+import { Context } from "../index.js";
+
 
 const Admin = () => {
+  const {card} = useContext(Context);
   const [cardVisible, setCardVisible] = useState(false);
   const [levelVisible, setLevelVisible] = useState(false);
   const [cardTypeVisible, setCardTypeVisible] = useState(false);
   const [themeVisible, setThemeVisible] = useState(false);
+
+
+
+  const lastCard = () => {
+  const cardData = card.cards.slice(); 
+  if (cardData.length > 0) {
+    const lastObject = cardData[cardData.length - 1];
+    const lastObjectImage = lastObject.image;
+    return lastObjectImage;
+  }
+  return null;
+  };
 
   return (
     <Container className="d-flex flex-column position-relative z-index-10">
@@ -58,6 +73,15 @@ const Admin = () => {
         show={cardVisible}
         onHide={() => setCardVisible(false)}
       />
+      <h2 className="lastCard-heading">Последняя добавленная карточка</h2>
+      <div className="lastCard">
+        {lastCard() 
+        ? 
+        <img src={"http://localhost:5000/" + lastCard()} width="204" height="144" alt=""/>
+        :
+        <img src="/assets/img/no-card.jpg" width="204" height="144" alt=""/>
+        }
+      </div>
     </Container>
   );
 };
